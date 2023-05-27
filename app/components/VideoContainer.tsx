705257f60acbac35b76d7312bab6fc382d15ab38
Tomoki <tomoki_ota@outlook.com>
2023-05-28 05:43:11 +0900

    added try, error for videocontainer video fetch.

--- app/components/VideoContainer.tsx
+++ app/components/VideoContainer.tsx
@@ -5,17 +5,36 @@ type Props = {
 };
 
 async function VideoContainer({ promise }: Props) {
-    const videos = await promise;
+    try {
+        const videos = await promise;
 
-    return (
-        <div
-            className={`grid grid-cols-[repeat(auto-fill,minmax(184px,_1fr))] gap-y-6 gap-x-4 min-w-[520px]`}
-        >
-            {videos.items.map((video) => (
-                <VideoCard video={video} key={video.id.videoId} />
-            ))}
-        </div>
-    );
+        if (!videos || !videos.items) {
+            return (
+                <div>
+                    <h1 className="text-4xl">
+                        Could not load videos at this time
+                    </h1>
+                </div>
+            );
+        }
+
+        return (
+            <div
+                className={`grid grid-cols-[repeat(auto-fill,minmax(184px,_1fr))] gap-y-6 gap-x-4 min-w-[520px]`}
+            >
+                {videos.items.map((video) => (
+                    <VideoCard video={video} key={video.id.videoId} />
+                ))}
+            </div>
+        );
+    } catch (err) {
+        console.error(err);
+        return (
+            <div>
+                <h1 className="text-4xl">Could not load videos at this time</h1>
+            </div>
+        );
+    }
 }
 
 export default VideoContainer;
